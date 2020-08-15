3d7d345a8009ced41dc7abfcdce06c3b2ca8eabe
Diego Miguel <dml1001@alu.ubu.es>
2020-08-15 21:45:56 +0900

    Parse participants

--- app/src/main/java/com/example/instadmviewer/SelectFile.java
+++ app/src/main/java/com/example/instadmviewer/SelectFile.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class SelectFile extends AppCompatActivity {
@@ -25,6 +27,13 @@ public class SelectFile extends AppCompatActivity {
     private static final int PICK_MESSAGE_FILE = 2;
 
     private ArrayList<Conversation> conversations;
+
+    // Strings used in the Instagram JSON file
+    private static final String PARTICIPANTS = "participants";
+    private static final String CONVERSATION = "conversation";
+    private static final String SENDER = "sender";
+    private static final String CREATED_AT = "created_at";
+    private static final String TEXT = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +44,15 @@ public class SelectFile extends AppCompatActivity {
     public void openFile(View view) {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("application/json");
+       // intent.setType("application/json"); // TODO: couldn't make this line work :(
+        intent.setType("*/*");
 
         startActivityForResult(intent, PICK_MESSAGE_FILE);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
         if (requestCode == PICK_MESSAGE_FILE && resultCode == Activity.RESULT_OK) {
             if (resultData != null) {
                 Uri uri = resultData.getData();
@@ -76,8 +87,18 @@ public class SelectFile extends AppCompatActivity {
 
     private void parseJsonFile(String messageFile) {
         try {
-            JSONObject jsonObject = new JSONObject(messageFile);
-            
+            List<String> participantList = new ArrayList<>();
+            JSONArray allConversations = new JSONArray(messageFile); // conversations array
+            for (int i = 0; i < allConversations.length(); i++) {
+                JSONObject singleConversation = allConversations.getJSONObject(i);
+                JSONArray participants = singleConversation.getJSONArray(PARTICIPANTS);
+                for (int j = 0; j < participants.length(); j++) {
+                    participantList.add((String) participants.get(j));
+                }
+                // TODO: from participant list, get the most frequent participant to infer which
+                // is the user's username
+                // TODO: *****************************
+            }
         } catch (JSONException e) {
             // TODO: display error
         }
